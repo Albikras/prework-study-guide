286a5bbaac1967a363afaca953988492d9fe4d67
Albikras <124819029+Albikras@users.noreply.github.com>
2023-04-23 13:28:30 -0400

    <added java script to study guide including functions and arrays with a math.random exectution>

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,29 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
-function listTopics(){   
-    for(x=0; x<topics.length; x++){
+var randomTopic =topics[Math.floor(Math.random()*topics.length)];
+
+
+console.log('Here are the topics we learned through Prework:');
+function listTopic(){   
+    for(var x=0; x<topics.length; x++){
     console.log(topics[x]);
     }   
 }
-function selectTopics(){
-    if(topics == 'HTML'){
-        console.log("lets study HTML");
-}   else if(topics == 'CSS'){
-        console.log("lets study CSS");
-}   else if(topics == 'Git'){
-        console.log("lets study Git");
-}   else if (topics == 'JavaScript'){
-        console.log("lets study JavaScript");
+
+
+
+function selectTopic(){
+    if(randomTopic == 'HTML'){
+        console.log("lets study HTML!");
+}   else if(randomTopic == 'CSS'){
+        console.log("lets study CSS!");
+}   else if(randomTopic == 'Git'){
+        console.log("lets study Git!");
+}   else if (randomTopic == 'JavaScript'){
+        console.log("lets study JavaScript!");
 }   else{
-        console.log("Please Try Again");
+        console.log("Please Try Again!");
 }
 }
+listTopic()
+console.log('Which topic should we study first?');
+selectTopic()
